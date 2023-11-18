d9be52c25adac23b71b921a93c681c4d95cee1e9
hungsvdut2k2 <hungsvdut@gmail.com>
2023-11-18 14:18:25 +0700

    chore: remove stuff in __init__

--- src/crawler/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_crawler import BaseCrawler
-from .crawler_arguments import CrawlerArguments
-from .vnexpress_crawler import VnExpressCrawler
--- src/driver/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base_driver import BaseDriver
-from .driver_config import DriverConfig
-from .chrome_driver import ChromeDriver
