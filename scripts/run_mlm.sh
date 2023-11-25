f6a3fb66e0d55630c13d15faefe39c3ea0d9c726
hungsvdut2k2 <hungsvdut@gmail.com>
2023-11-25 08:56:42 +0700

    chore: update bash scripts

--- scripts/run_mlm.sh
+++ scripts/run_mlm.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH=./
+export PYTHONPATH=./ 
 python src/masked_language_modelling.py \
 --model-name hungsvdut2k2/vietnamese-xlm-roberta \
 --data-directory /Users/viethungnguyen/9h53-Sportivefy/dataset \
--- scripts/run_vector_db.sh
+++ scripts/run_vector_db.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH=./
+export PYTHONPATH=./ 
 python src/build_vector_database.py \
 --json-file-path /Users/viethungnguyen/9h53-Sportivefy/weights/vnexpress.json \
 --model-name hungsvdut2k2/vietnamese-xlm-roberta \
