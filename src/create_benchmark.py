a75c8334d699ea6e146ab90dfd30789ae0f9e6b6
hungsvdut2k2 <hungsvdut@gmail.com>
2023-12-03 11:00:04 +0700

    feat: add append mode for create benchmark

--- src/create_benchmark.py
+++ src/create_benchmark.py
@@ -31,9 +31,10 @@ if __name__ == "__main__":
     parser.add_argument("--output-dir", type=str, required=True)
     parser.add_argument("--valid-file-path", type=str, default=None)
     args = parser.parse_args()
-    dataframe = {"object_id": [], "query": [], "index": []}
+    dataframe = pd.DataFrame()
     json_content = json.load(open(args.json_file_path))
     if args.valid_file_path:
+        dataframe = pd.read_csv(args.valid_file_path)
         indices = pd.read_csv(args.valid_file_path)["index"].tolist()
         logger.info("Start Remove Stuff")
         for index in tqdm(sorted(indices, reverse=True)):
@@ -42,12 +43,13 @@ if __name__ == "__main__":
     logger.info("Start Generate Query")
     for i in tqdm(range(len(json_content))):
         try:
+            new_row = {}
             generated_prompt = generate_prompt(json_content=json_content[i]["article"])
             response = get_response(generate_prompt(generated_prompt))
-            dataframe["object_id"].append(json_content[i]["_id"]["$oid"])
-            dataframe["query"].append(response)
-            dataframe["index"].append(i)
+            new_row["object_id"] = json_content[i]["_id"]["$oid"]
+            new_row["query"] = response
+            new_row["index"] = len(dataframe) + i
+            dataframe = dataframe._append(new_row, ignore_index=True)
         except KeyboardInterrupt:
-            df = pd.DataFrame(dataframe)
-            df.to_csv(os.path.join(args.output_dir, "query.csv"), index=False)
+            dataframe.to_csv(os.path.join(args.output_dir, "query.csv"), index=False)
             break
